fca4e20f4ff6f81b6bc0803334745080a77d210b
Jules van Enckevort <j.vanenckevort@vwt.digital>
2019-10-17 18:12:28 +0200

    Added pre-commit hooks for .json .py and .sh scripts

---
+++ README.md
@@ -0,0 +1,9 @@
+# pre-commit
+
+This repository contains pre-commit scripts that can be installed globally by running the following command:
+
+```
+./install.sh
+```
+
+Both Linux and OSX are supported.
---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Installing commit hooks, depending on your operating system.
+#
+
+os="$(uname -s)"
+case "${os}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${os}"
+esac
+
+echo "Installing commit hooks for ${machine}..."
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if [ "$machine" = "Linux" ]; then
+    echo "Installing packages for ${machine}..."
+    apt-get install -y shellcheck
+    for file in "$DIR"/hooks/*; do
+        echo "$file"
+        sed -i 's/\-E/\-P/g' "${file}"
+    done
+elif [ "$machine" = "Mac" ]; then
+    echo "Installing packages for ${machine}..."
+    brew install shellcheck
+fi
+
+git config --global core.hooksPath "${DIR}"/hooks
+echo "Configured global core.hooksPath: ${DIR}/hooks"
+
+echo "Installing other required packages for git hooks..."
+if ! command -v flake8; then
+  echo "Installing flak8..."
+  pip install flake8
+fi
+
+
