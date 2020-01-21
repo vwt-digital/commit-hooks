93cac361b338cad48d8a4db5a90824e3f0649dcd
Eric van der Velde <e.vandervelde@vwt.digital>
2020-01-21 08:24:41 +0100

    Assume YES when installing bandit. DAT-1662

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ if [ "$machine" = "Linux" ]; then
     echo "Installing shellcheck..."
     apt-get install -y shellcheck
     echo "Installing bandit..."
-    apt-get install python3-bandit
+    apt-get install -y python3-bandit
     for file in "$DIR"/hooks/*; do
         echo "$file"
         sed -i 's/\-E/\-P/g' "${file}"
