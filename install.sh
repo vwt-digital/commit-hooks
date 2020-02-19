15963e1a8d48c141807d0c7bea1ed3c222baf539
David Strootman <david.strootman@gmail.com>
2020-02-19 13:21:50 +0100

    DAT-1899: Replace scan tools with sast-scan on Linux

--- install.sh
+++ install.sh
@@ -17,21 +17,25 @@ echo "Installing commit hooks for ${machine}..."
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 if [ "$machine" = "Linux" ]; then
-    echo "Installing packages for ${machine}..."
-    echo "Installing shellcheck..."
-    apt-get install -y shellcheck
-    echo "Installing bandit..."
-    pip3 install bandit
+  echo "Installing packages for ${machine}..."
+  echo "Cloning sast-scan..."
+  git clone https://github.com/vwt-digital/cloudbuilder-sast
+  mv cloudbuilder-sast/docker-sast.sh /opt/sast-scan.sh
+  rm -rf cloudbuilder-sast/
+#    echo "Installing packages for ${machine}..."
+#    echo "Installing shellcheck..."
+#    apt-get install -y shellcheck
+#    echo "Installing bandit..."
+#    pip3 install bandit
 elif [ "$machine" = "Mac" ]; then
-    echo "Installing packages for ${machine}..."
-    brew install shellcheck
+  echo "Installing packages for ${machine}..."
+  brew install shellcheck
+  if ! command -v flake8; then
+      echo "Installing flak8..."
+      pip3 install flake8
+  fi
 fi
 
 git config --global core.hooksPath "${DIR}"/hooks
 echo "Configured global core.hooksPath: ${DIR}/hooks"
 
-echo "Installing other required packages for git hooks..."
-if ! command -v flake8; then
-  echo "Installing flak8..."
-  pip3 install flake8
-fi
