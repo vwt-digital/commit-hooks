136a17d2b23e424958bb909925980ecb1e34631d
David Strootman <david.strootman@gmail.com>
2020-02-25 14:47:30 +0100

    DAT-1989: Fix wrong error showing when Docker run fails

--- install.sh
+++ install.sh
@@ -3,23 +3,14 @@
 # Installing commit hooks, depending on your operating system.
 #
 
-os="$(uname -s)"
-case "${os}" in
-    Linux*)     machine=Linux;;
-    Darwin*)    machine=Mac;;
-    CYGWIN*)    machine=Cygwin;;
-    MINGW*)     machine=MinGw;;
-    *)          machine="UNKNOWN:${os}"
-esac
-
 echo "Installing commit hooks for ${machine}..."
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-d$()ksjlaf[25-{}%$DK@@@]
+
 echo "Installing SAST-scan..."
 if ! docker pull eu.gcr.io/vwt-d-gew1-dat-cloudbuilders/cloudbuilder-sast:latest ; then
   echo -e "\e[1;31mError: Install wasn't executed with sufficient permissions or Docker is not configured for gcloud." \
-          "Run 'gcloud auth configure-docker' to do so. \e[0m"
+          "Run 'gcloud auth configure-docker' to configure Docker."
 fi
 
 git config --global core.hooksPath "${DIR}"/hooks
