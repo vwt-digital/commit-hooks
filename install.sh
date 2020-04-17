de5be4f13162980b9cd15318787ce750a215f7be
David Strootman <david.strootman@gmail.com>
2020-04-17 08:22:09 +0200

    DAT-2637: Clean error/log messages and remove duplicate

--- install.sh
+++ install.sh
@@ -14,12 +14,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo "Installing SAST-scan..."
 if ! docker pull eu.gcr.io/vwt-p-gew1-dat-cloudbuilders/cloudbuilder-sast:latest; then
-  echo -e "\e[1;31mError: Install wasn't executed with sufficient permissions or Docker is not configured for gcloud." \
-          "You might need to run 'gcloud auth configure-docker' to configure Docker. \e[0m"
-fi
-if ! docker pull eu.gcr.io/vwt-d-gew1-dat-cloudbuilders/cloudbuilder-sast:latest; then
-  echo -e "\e[1;31mError: Install wasn't executed with sufficient permissions or Docker is not configured for gcloud." \
-          "You might need to run 'gcloud auth configure-docker' to configure Docker. \e[0m"
+  echo -e "\e[1;31mError: Install wasn't executed with sufficient permissions or Docker is not configured for" \
+          "gcloud\e[0m"
+else
+  docker pull eu.gcr.io/vwt-d-gew1-dat-cloudbuilders/cloudbuilder-sast:latest
 fi
 
 git config --global core.hooksPath "${DIR}"/hooks
